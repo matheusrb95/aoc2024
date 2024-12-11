dc64d3ce81e1e417887f9bf74666a813367a9216
Matheus <matheusrenan.95@gmail.com>
2024-12-11 16:02:13 -0300

    feat: day 8 part 2

--- d8/main.go
+++ d8/main.go
@@ -28,72 +28,37 @@ func run() int {
 	}
 
 	uniqueAntinodes := make(map[Pos]bool)
+	antinodes := make([]Pos, 0)
 
 	for _, v := range antennas {
 		combinations := combinations(v)
 
 		for _, combination := range combinations {
-			antinodes := antinode(combination[0], combination[1])
-			for _, pos := range antinodes {
-				uniqueAntinodes[pos] = true
-			}
+			antinodes = append(antinodes, combination[0])
+			antinodes = append(antinodes, combination[1])
+			ressonantHarmonic(combination[0], combination[1], &antinodes)
+			ressonantHarmonic(combination[1], combination[0], &antinodes)
 		}
 	}
 
-	fmt.Println(uniqueAntinodes)
-
-	var result int
-	for pos := range uniqueAntinodes {
-		if !insideBoundary(pos) {
-			continue
-		}
-
-		result++
+	for _, pos := range antinodes {
+		uniqueAntinodes[pos] = true
 	}
 
-	fmt.Println(result)
+	fmt.Println(len(uniqueAntinodes))
 
 	return 0
 }
 
-func insideBoundary(pos Pos) bool {
-	if pos.row < 0 || pos.col < 0 {
-		return false
+func ressonantHarmonic(a1, a2 Pos, antinodes *[]Pos) {
+	antinode := Pos{row: a1.row + (a1.row - a2.row), col: a1.col + (a1.col - a2.col)}
+
+	if antinode.row < 0 || antinode.col < 0 || antinode.row >= lenRow || antinode.col >= lenCol {
+		return
 	}
 
-	if pos.row >= lenRow || pos.col >= lenCol {
-		return false
-	}
-
-	return true
-}
-
-func antinode(a1, a2 Pos) []Pos {
-	result := make([]Pos, 0)
-	diff := diffPoints(a1, a2)
-
-	if (a1.row > a2.row && a1.col > a2.col) || (a2.row > a1.row && a2.col > a1.col) {
-		result = append(result, Pos{row: a1.row - diff.row, col: a1.col - diff.col})
-		result = append(result, Pos{row: a2.row + diff.row, col: a2.col + diff.col})
-
-		return result
-	}
-
-	result = append(result, Pos{row: a1.row - diff.row, col: a1.col + diff.col})
-	result = append(result, Pos{row: a2.row + diff.row, col: a2.col - diff.col})
-
-	return result
-}
-
-func diffPoints(a, b Pos) Pos {
-	return Pos{row: abs(a.row - b.row), col: abs(a.col - b.col)}
-}
-
-func abs(num int) int {
-	if num < 0 {
-		return num * -1
-	}
-	return num
+	*antinodes = append(*antinodes, antinode)
+	ressonantHarmonic(antinode, a1, antinodes)
 }
 
 func combinations(antennas []Pos) [][]Pos {
